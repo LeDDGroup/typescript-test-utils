26d55adaa427b3dd8c7c8642b42ce738babe834d
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-11-21 18:13:54 -0500

    chore: maybe improve assert type inference

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,14 +10,20 @@ import {
 
 type DataType = { a: string; b: string };
 
+// HasProperties
 assert<HasProperties<DataType, "a">>(true);
 assert<HasProperties<DataType, "a" | "b">>(true);
 assert<HasProperties<DataType, "a" | "b" | "c">>(false);
 assert<HasProperties<DataType, "c">>(false);
+
+// Extends
 assert<Extends<DataType, { c: string }>>(false);
 assert<Extends<DataType, { a: string; b: string }>>(true);
+
+//Equals
 assert<Equals<DataType, { a: string; b: string }>>(true);
 assert<Not<Equals<DataType, { a: string; b: string }>>>(false);
 
+// Extra
 assertTrue<Equals<DataType, { a: string; b: string }>>();
 assertFalse<Extends<DataType, { c: string }>>();
