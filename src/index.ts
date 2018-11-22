b6f43bdbcbd641da7004cd72163643bb1581920f
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-11-21 23:44:38 -0500

    refact: add type helpers
    - If
    - And

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-type IndexType = string | number | symbol;
-type ObjectWithProperties<K extends IndexType> = { [id in K]: any };
-export type Equals<T, K> = T extends K ? (K extends T ? true : false) : false;
-export type Extends<T, K> = T extends K ? true : false;
-export type HasProperties<T, K extends IndexType> = Extends<
-  T,
-  ObjectWithProperties<K>
+type ObjectMap<K extends keyof any = keyof any, V = any> = { [id in K]: V };
+
+type If<Condition extends boolean, Then, Else = never> = Condition extends true
+  ? Then
+  : Else;
+type And<A extends boolean, B extends boolean> = If<
+  A,
+  If<B, true, false>,
+  false
 >;
-export type Not<T extends boolean> = T extends true ? false : true;
+export type Extends<A, B> = A extends B ? true : false;
+export type HasProperties<T, K extends keyof any> = Extends<T, ObjectMap<K>>;
+export type Not<T extends boolean> = If<Extends<T, true>, false, true>;
+export type Equals<A, B> = And<Extends<A, B>, Extends<B, A>>;
 
 export function assert<T extends boolean>(expected: T) {}
 export function assertTrue<T extends true>() {}
